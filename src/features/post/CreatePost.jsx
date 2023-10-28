fb55465472790834fcc792a8344deb995525a4ac
pornrapat309 <pornrapat.kaew@gmail.com>
2023-10-28 22:02:57 +0700

    feature create post

--- src/features/post/CreatePost.jsx
+++ src/features/post/CreatePost.jsx
@@ -1,19 +1,42 @@
+import axios from "../../config/axios";
 import { useState, useRef } from "react";
 import { HiPhoto } from "react-icons/hi2";
 
 export default function CreatePost() {
   const [file, setFile] = useState(null);
+  const [message, setMessage] = useState("");
   const fileEl = useRef(null);
+
+  const handleSubmitPost = async (e) => {
+    try {
+      e.preventDefault();
+      const formData = new FormData();
+      if (file) {
+        formData.append("image", file);
+      }
+      if (message) {
+        formData.append("message", message);
+      }
+      await axios.post("/post", formData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       <div className="flex justify-between h-80">
         <div
-          className="flex flex-1 flex-col justify-center items-center border border-r cursor-pointer overflow-hidden hover:bg-blue-900"
+          className="flex flex-1 flex-col justify-center items-center border border-r cursor-pointer  hover:bg-blue-900 overflow-hidden"
           onClick={() => fileEl.current.click()}
         >
           {file ? (
-            <div onClick={fileEl.current.click()}>
-              <img src={URL.createObjectURL(file)} alt="post" />
+            <div onClick={() => fileEl.current.click()}>
+              <img
+                src={URL.createObjectURL(file)}
+                alt="post"
+                className="w-full h-full"
+              />
             </div>
           ) : (
             <HiPhoto className="h-10 w-10" />
@@ -29,14 +52,20 @@ export default function CreatePost() {
             }}
           />
         </div>
+
         <div className="flex-1">
           <textarea
-            className="w-full h-full resize-none bg-blue-950"
+            className="block w-full h-full resize-none bg-blue-950"
             placeholder="Write about your sky..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           ></textarea>
         </div>
       </div>
-      <div className="flex justify-center border-t text-base font-bold min-w-full text-blue-500 p-3 cursor-pointer">
+      <div
+        className="flex justify-center border-t text-base font-bold min-w-full text-blue-500 p-3 cursor-pointer"
+        onClick={handleSubmitPost}
+      >
         Post
       </div>
     </div>
